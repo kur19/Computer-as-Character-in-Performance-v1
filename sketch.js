28289026c3c2e245a31b1634977c9027800f9a39
Michael Musick <m.a.musick@gmail.com>
2019-04-10 17:36:04 -0600

    mouse click causes "startListener" not to work when called from speech.onEnd()

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var der;
 
 let agent = {};
 
+// if selffeedback mode, then use continuous listening
 let selfFeedback = false;
 
 
@@ -13,8 +14,8 @@ function setup() {
     console.log("in standby mode, click mouse to start system!");
 }
 
-function draw(){
-    let node = document.querySelector('#testDiv');
+function draw() {
+    let node = document.querySelector('#agent_status');
 
     node.innerText = agent.state;
 }
@@ -30,56 +31,57 @@ function mousePressed() {
 
 function speechSetup() {
     let speech = new p5.Speech();
-    let kickoffSpeech = new p5.Speech();
     let listener = new p5.SpeechRec('en-US', gotSpeech);
     let continuous = false;
 
-    if( selfFeedback ){
+    if (selfFeedback) {
         continuous = true;
         console.log("self feedback mode is on");
     }
 
     let interim = false;
-    // listener.start(continuous, interim);
+
     listener.continuous = continuous;
     listener.interim = interim;
-
-    // start the listener
-    function startListener(){
-        if( agent.state !== 'listening'){
-            listener.start();
-        }
-    }
 
 
     // ***********************CALLBACK FUNCTIONS*****************************
 
+    // start the listener
+    function startListener() {
+        if (agent.state !== 'listening') {
+            console.log("started listener");
+            listener.start();
+        }
+    }
+
     listener.onStart = function() {
         // console.log("I am listening...");
-        agent.state = "listening";
+        agent.state = 'listening';
     };
     listener.onEnd = function() {
-        // console.log("I stopped listening!!!!!!!");
-        if( !selfFeedback ){ agent.state = undefined; }
+        console.log("I stopped listening!!!!!!!");
+        if (!selfFeedback) { agent.state = undefined; }
     };
 
     // function to execute when speaking starts
     speech.onStart = function() {
-        // console.log("started...");
-        agent.state = "speaking";
+        console.log("started talking...");
+        agent.state = 'speaking';
     };
 
     // function to execute when speaking stops
     speech.onEnd = function() {
-        // console.log("stopped talking...");
-        if( !selfFeedback ){ agent.state = undefined; }
+        console.log("stopped talking...");
+        if (!selfFeedback) { agent.state = undefined; }
         // restart listener
-        if( !selfFeedback ){ startListener(); console.log("test"); }
+        if (!selfFeedback) { startListener(); console.log("stop talking, start listening"); }
 
     };
 
 
 
+    // ***********************RIVE BOT*****************************
 
     let bot = new RiveScript();
 
@@ -102,22 +104,28 @@ function speechSetup() {
 
     function gotSpeech() {
         if (listener.resultValue) {
+
+            // get user spoken string and pass along
             let input = listener.resultString;
             console.log(input);
-            //user_input.value(input);
+            user_input.html(input);
+
+            // get the reply from rive script
+            // then hand it to the synthesizer
             bot.reply("local-user", input).then(function(reply) {
+
                 listener.rec.abort();
                 speech.speak(reply);
-                // output.html(reply);
+                output.html(reply);
                 console.log(reply);
-                clearTimeout(silenceCheckTimer);
-                silenceCheckTimer = setTimout(function() {
-                    listener.rec.abort();
-                    agent.state = "speaking";
-                    kickoffSpeech.speak(kickoffStatements[2]);
-                  },
-                  8000
-                );
+                // clearTimeout(silenceCheckTimer);
+                // silenceCheckTimer = setTimout(function() {
+                //     listener.rec.abort();
+                //     agent.state = "speaking";
+                //     kickoffSpeech.speak(kickoffStatements[2]);
+                // },
+                //     8000
+                // );
 
             });
         }
@@ -125,15 +133,15 @@ function speechSetup() {
 
 
     // timeouts to check agent state
-    function checkAgentState() {
-        if( typeof agent.state === 'undefined' ){
-            console.log("agent is off....");
-
-            // restart you engines!
-            startListener();
-        }
-    }
-    let agentCheckTimer = setInterval(checkAgentState, 1000);
+    // function checkAgentState() {
+    //     if (typeof agent.state === 'undefined') {
+    //         console.log("agent is off....");
+    //
+    //         // restart you engines!
+    //         startListener();
+    //     }
+    // }
+    // let agentCheckTimer = setInterval(checkAgentState, 1000);
 
 
     // START THE PROCESS!!!!
@@ -143,10 +151,6 @@ function speechSetup() {
     //let input = user_input.value();
 
     //}
-
-
-
-    der = listener;
 }
 
 
@@ -154,7 +158,7 @@ function speechSetup() {
 
 
 let kickoffStatements = [
-  "where did you go?",
-  "relax I'm still here."
+    "where did you go?",
+    "relax I'm still here."
 
 ];
